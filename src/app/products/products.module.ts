0f8db5dd12df6686b28b24cfdf48277df9ee03e5
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-06 13:55:49 +0200

    "278"

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products.routing';
+import { ProductComponent } from './pages/product/product.component';
+import { PriceComponent } from './components/price/price.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+  ],
+  declarations: [
+    ProductComponent,
+    PriceComponent
+  ]
+})
+export class ProductsModule { }
